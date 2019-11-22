6a647ae8c5bc117c4424587b6fad74dc698ff887
Rex Yu <yu.chenhang@gmail.com>
2019-11-22 17:52:38 +0800

    增加 MyBatis configuration

---
+++ my-dao/src/main/java/com/rex/my/dao/config/MyBatisConfig.java
@@ -0,0 +1,59 @@
+package com.rex.my.dao.config;
+
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+import javax.sql.DataSource;
+import javax.transaction.UserTransaction;
+
+@Configuration
+@MapperScan(basePackages = {"com.rex.my.entity.mysql"},
+            sqlSessionTemplateRef = "mysqlSqlSessionTemplate")
+public class MyBatisConfig {
+
+    @Bean(name = "mysqlDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.mysql")
+    public DataSource mysqlDataSource() {
+        return new AtomikosDataSourceBean();
+    }
+
+    @Bean(name = "transactionManager")
+    @Primary
+    public JtaTransactionManager transactionManager() {
+        UserTransactionManager userTransactionManager = new UserTransactionManager();
+        UserTransaction userTransaction = new UserTransactionImp();
+        return new JtaTransactionManager(userTransaction, userTransactionManager);
+    }
+
+    @Bean(name = "mysqlSqlSessionFactory")
+    @Primary
+    public SqlSessionFactory mysqlSqlSessionFactory(@Qualifier("mysqlDataSource") DataSource mysqlDataSource)
+            throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(mysqlDataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/mysql/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean(name = "mysqlSqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate mysqlSqlSessionTemplate(
+            @Qualifier("mysqlSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
