0fd0cfc3944edc2275ae623b03fda6ad9ea30b4c
Rex Yu <yu.chenhang@gmail.com>
2019-11-25 15:43:18 +0800

    @SpringBootApplication

---
+++ my-business/src/main/java/com/rex/MyBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.rex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBusinessApplication.class);
+    }
+
+}
