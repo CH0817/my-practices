569a24656ed15c4e6699dad3a5e2e5eb0bf53972
Rex Yu <yu.chenhang@gmail.com>
2019-11-26 16:27:48 +0800

    註解沒用到的 annotation

--- my-dao/src/main/java/com/rex/my/dao/config/property/MySqlProperties.java
+++ my-dao/src/main/java/com/rex/my/dao/config/property/MySqlProperties.java
@@ -3,16 +3,14 @@ package com.rex.my.dao.config.property;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @ToString
 @Component
-@PropertySource("classpath:mysql-config.properties")
-@ConfigurationProperties(prefix = "datasource.mysql")
+// @PropertySource("classpath:mysql-config.properties")
+// @ConfigurationProperties(prefix = "datasource.mysql")
 public class MySqlProperties {
 
     private String driverClass;
