75b7e02f684c2c5fb57144874672b481c14e33c7
Rex Yu <yu.chenhang@gmail.com>
2019-11-27 09:49:58 +0800

    刪除沒用到的程式碼

--- my-dao/src/main/java/com/rex/my/dao/config/PrimaryMyBatisConfig.java
+++ my-dao/src/main/java/com/rex/my/dao/config/PrimaryMyBatisConfig.java
@@ -1,7 +1,5 @@
 package com.rex.my.dao.config;
 
-import com.atomikos.icatch.jta.UserTransactionImp;
-import com.atomikos.icatch.jta.UserTransactionManager;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -13,10 +11,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.transaction.jta.JtaTransactionManager;
 
 import javax.sql.DataSource;
-import javax.transaction.UserTransaction;
 
 @Configuration
 @MapperScan(basePackages = {"com.rex.my.dao.mapper.mysql"}, sqlSessionTemplateRef = "primarySqlSessionTemplate")
@@ -27,14 +23,6 @@ public class PrimaryMyBatisConfig {
     @ConfigurationProperties(prefix = "datasource.primary")
     public DataSource primaryDataSource() {
         return new AtomikosDataSourceBean();
-    }
-
-    @Bean(name = "transactionManager")
-    @Primary
-    public JtaTransactionManager transactionManager() {
-        UserTransactionManager userTransactionManager = new UserTransactionManager();
-        UserTransaction userTransaction = new UserTransactionImp();
-        return new JtaTransactionManager(userTransaction, userTransactionManager);
     }
 
     @Bean(name = "primarySqlSessionFactory")
@@ -54,51 +42,5 @@ public class PrimaryMyBatisConfig {
             @Qualifier("primarySqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
-
-    // private final MySqlProperties mySqlProperties;
-    //
-    // @Autowired
-    // public MyBatisConfig(MySqlProperties mySqlProperties) {
-    //     this.mySqlProperties = mySqlProperties;
-    // }
-    //
-    // @Bean(name = "mysqlDataSource")
-    // @Primary
-    // public DataSource mysqlDataSource() {
-    //     Properties properties = new Properties();
-    //     properties.setProperty("url", mySqlProperties.getUrl());
-    //     properties.setProperty("user", mySqlProperties.getUser());
-    //     properties.setProperty("password", mySqlProperties.getPassword());
-    //     AtomikosDataSourceBean dataSourceBean = new AtomikosDataSourceBean();
-    //     dataSourceBean.setXaDataSourceClassName(mySqlProperties.getDriverClass());
-    //     dataSourceBean.setXaProperties(properties);
-    //     return dataSourceBean;
-    // }
-    //
-    // @Bean(name = "transactionManager")
-    // @Primary
-    // public JtaTransactionManager transactionManager() {
-    //     UserTransactionManager userTransactionManager = new UserTransactionManager();
-    //     UserTransaction userTransaction = new UserTransactionImp();
-    //     return new JtaTransactionManager(userTransaction, userTransactionManager);
-    // }
-    //
-    // @Bean(name = "primarySqlSessionFactory")
-    // @Primary
-    // public SqlSessionFactory primarySqlSessionFactory(@Qualifier("mysqlDataSource") DataSource mysqlDataSource)
-    //         throws Exception {
-    //     final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-    //     sessionFactory.setDataSource(mysqlDataSource);
-    //     sessionFactory.setMapperLocations(
-    //             new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/mysql/*.xml"));
-    //     return sessionFactory.getObject();
-    // }
-    //
-    // @Bean(name = "primarySqlSessionTemplate")
-    // @Primary
-    // public SqlSessionTemplate primarySqlSessionTemplate(
-    //         @Qualifier("primarySqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-    //     return new SqlSessionTemplate(sqlSessionFactory);
-    // }
 
 }
