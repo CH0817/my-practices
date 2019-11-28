782fb0389b90c570b3047c1953d13aafca022dc4
Rex Yu <yu.chenhang@gmail.com>
2019-11-28 17:03:54 +0800

    新增 BaseMapperTest.java

---
+++ my-dao/src/test/java/com/rex/my/dao/mapper/primary/base/BaseMapperTest.java
@@ -0,0 +1,13 @@
+package com.rex.my.dao.mapper.primary.base;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@Ignore
+public class BaseMapperTest {}
