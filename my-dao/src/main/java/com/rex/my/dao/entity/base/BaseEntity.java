9250b9475eca0fd30323e4924b8e2ce696861042
Rex Yu <yu.chenhang@gmail.com>
2019-11-28 17:03:25 +0800

    修正 toString() 為正常顯示的錯誤

--- my-dao/src/main/java/com/rex/my/dao/entity/base/BaseEntity.java
+++ my-dao/src/main/java/com/rex/my/dao/entity/base/BaseEntity.java
@@ -9,7 +9,7 @@ public class BaseEntity implements Serializable {
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(ToStringStyle.MULTI_LINE_STYLE);
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 
 }
