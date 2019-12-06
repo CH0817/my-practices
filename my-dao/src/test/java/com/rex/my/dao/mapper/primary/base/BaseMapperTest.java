f753a50a6b9f08c04ab3b03ef1713aad21122d7d
Rex Yu <yu.chenhang@gmail.com>
2019-12-06 16:56:18 +0800

    修改 base 測試設定

--- my-dao/src/test/java/com/rex/my/dao/mapper/primary/base/BaseMapperTest.java
+++ my-dao/src/test/java/com/rex/my/dao/mapper/primary/base/BaseMapperTest.java
@@ -2,12 +2,18 @@ package com.rex.my.dao.mapper.primary.base;
 
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Transactional
 @Ignore
-public class BaseMapperTest {}
+public class BaseMapperTest {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+}
