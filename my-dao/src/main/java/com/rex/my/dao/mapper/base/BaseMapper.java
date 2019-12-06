8cf17ad4531a45b9dd1af84474ac4571517d4992
Rex Yu <yu.chenhang@gmail.com>
2019-12-06 17:54:31 +0800

    移動通用的 mapper 方法至 BaseMapper

---
+++ my-dao/src/main/java/com/rex/my/dao/mapper/base/BaseMapper.java
@@ -0,0 +1,17 @@
+package com.rex.my.dao.mapper.base;
+
+import java.util.List;
+
+public interface BaseMapper<E> {
+
+    int insertSelective(E entity);
+
+    List<E> selectAll();
+
+    E selectByPrimaryKey(String id);
+
+    int update2DeleteByPrimaryKey(String id);
+
+    int updateSelectiveByPrimaryKey(E entity);
+
+}
