a9eedd3da064563a79f592c75ea47d6aba544c9a
Rex Yu <yu.chenhang@gmail.com>
2019-12-08 17:37:50 +0800

    item basic CRUD were completed

--- my-dao/src/main/java/com/rex/my/dao/mapper/primary/ItemMapper.java
+++ my-dao/src/main/java/com/rex/my/dao/mapper/primary/ItemMapper.java
@@ -1,3 +1,6 @@
 package com.rex.my.dao.mapper.primary;
 
-public interface ItemMapper {}
+import com.rex.my.dao.mapper.base.BaseMapper;
+import com.rex.my.model.dao.primary.Item;
+
+public interface ItemMapper extends BaseMapper<Item> {}
---
+++ my-dao/src/test/java/com/rex/my/dao/mapper/primary/ItemMapperTest.java
@@ -0,0 +1,60 @@
+package com.rex.my.dao.mapper.primary;
+
+import com.rex.my.dao.mapper.primary.base.BaseMapperTest;
+import com.rex.my.model.dao.primary.Account;
+import com.rex.my.model.dao.primary.Item;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class ItemMapperTest extends BaseMapperTest {
+
+    @Autowired
+    private ItemMapper mapper;
+
+    @Test
+    public void insertSelective() {
+        Date today = new Date();
+        Item entity = new Item();
+        entity.setName("測試類別");
+        entity.setUserId("a");
+        entity.setRemoved(Boolean.TRUE);
+        entity.setCreateDate(today);
+        entity.setUpdateDate(today);
+        int executeCount = mapper.insertSelective(entity);
+        logger.info("id: {}", entity.getId());
+        assertFalse(StringUtils.isEmpty(entity.getId()));
+        assertEquals(32, entity.getId().length());
+        assertEquals(1, executeCount);
+    }
+
+    @Test
+    public void selectAll() {
+        assertEquals(1, mapper.selectAll().size());
+    }
+
+    @Test
+    public void selectByPrimaryKey() {
+        assertEquals("a", mapper.selectByPrimaryKey("a").getId());
+    }
+
+    @Test
+    public void update2DeleteByPrimaryKey() {
+        assertEquals(1, mapper.update2DeleteByPrimaryKey("a"));
+    }
+
+    @Test
+    public void updateSelectiveByPrimaryKey() {
+        Item entity = mapper.selectByPrimaryKey("a");
+        entity.setUpdateDate(new Date());
+        int executeCount = mapper.updateSelectiveByPrimaryKey(entity);
+        assertEquals(1, executeCount);
+    }
+
+}
