830356c852967aee78739d606d328645f876360c
Rex Yu <yu.chenhang@gmail.com>
2019-12-08 16:47:06 +0800

    account_type basic CRUD were completed

--- my-dao/src/main/java/com/rex/my/dao/mapper/primary/AccountTypeMapper.java
+++ my-dao/src/main/java/com/rex/my/dao/mapper/primary/AccountTypeMapper.java
@@ -1,3 +1,6 @@
 package com.rex.my.dao.mapper.primary;
 
-public interface AccountTypeMapper {}
+import com.rex.my.dao.mapper.base.BaseMapper;
+import com.rex.my.model.dao.primary.AccountType;
+
+public interface AccountTypeMapper extends BaseMapper<AccountType> {}
