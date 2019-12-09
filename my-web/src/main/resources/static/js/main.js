9750a1c9aae84040cd62e093d3c13e0fd878df7e
Rex Yu <yu.chenhang@gmail.com>
2019-12-09 14:07:22 +0800

    調整功能主頁載入方式

--- my-web/src/main/resources/static/js/main.js
+++ my-web/src/main/resources/static/js/main.js
@@ -4,11 +4,11 @@ function initMenu() {
     $('#functionTree').tree({
         url: 'function/menu',
         onClick: function (node) {
-            console.info(node);
             let attributes = node.attributes;
             // FIXME 要過濾 logout url
             if (attributes !== undefined) {
-                $('body').layout('panel', 'center').panel('refresh', attributes.url);
+                $('body').layout('panel', 'center')
+                    .panel('refresh', attributes.url);
             }
         }
     });
