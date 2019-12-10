50d78686beb52e321d63874ac0677912b98a1391
Rex Yu <yu.chenhang@gmail.com>
2019-12-10 13:15:18 +0800

    增加 CRUD 操作、修改 column field

--- my-model/src/main/java/com/rex/my/model/easyui/grid/TradeGridVo.java
+++ my-model/src/main/java/com/rex/my/model/easyui/grid/TradeGridVo.java
@@ -1,33 +1,25 @@
 package com.rex.my.model.easyui.grid;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.rex.my.model.base.BaseModel;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Objects;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TradeGridVo extends BaseModel {
 
     private String id;
     private String accountId;
     private String itemId;
-    @JsonIgnore
     private BigDecimal money;
-    private String moneyString;
     private String tradeType;
     @JsonFormat(pattern = "yyyy/MM/dd")
     private Date tradeDate;
-    private String accountName;
-    private String itemName;
-
-    public String getMoneyString() {
-        return Objects.nonNull(money) ? money.toString() : "";
-    }
 
 }
--- my-web/src/main/resources/static/js/account_book.js
+++ my-web/src/main/resources/static/js/account_book.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function () {
     initGrid();
 });
@@ -12,6 +14,25 @@ function initGrid() {
         pageSize: 30,
         pageList: [10, 30, 50, 70, 100],
         columns: getGridColumns(),
+        onEndEdit: function (index, row) {
+            var ed = $(this).datagrid('getEditor', {
+                index: index,
+                field: 'productid'
+            });
+            row.productname = $(ed.target).combobox('getText');
+        },
+        onBeforeEdit: function (index, row) {
+            row.editing = true;
+            $(this).datagrid('refreshRow', index);
+        },
+        onAfterEdit: function (index, row) {
+            row.editing = false;
+            $(this).datagrid('refreshRow', index);
+        },
+        onCancelEdit: function (index, row) {
+            row.editing = false;
+            $(this).datagrid('refreshRow', index);
+        },
         onBeforeLoad: function (param) {
             switch (param.sort) {
                 case "moneyString":
@@ -33,36 +54,139 @@ function initGrid() {
 
 function getGridColumns() {
     return [[{
-        field: 'ck',
+        field: 'id',
         checkbox: true
     }, {
-        field: 'id',
-        hidden: true
-    }, {
-        field: 'accountId',
-        hidden: true
-    }, {
-        field: 'itemId',
-        hidden: true
-    }, {
-        field: 'moneyString',
+        field: 'money',
         title: '金額',
-        sortable: true
+        width: 120,
+        sortable: true,
+        editor: {
+            type: 'numberbox',
+            options: {
+                min: 0,
+                precision: 2
+            }
+        }
     }, {
         field: 'tradeType',
         title: '類型',
-        sortable: true
+        width: 80,
+        sortable: true,
+        editor: {
+            type: 'combobox',
+            options: {
+                valueField: 'id',
+                textField: 'name',
+                limitToList: true,
+                editable: false,
+                data: [{
+                    "id": 1,
+                    "name": "收入",
+                    "selected": true
+                }, {
+                    "id": 2,
+                    "name": "支出"
+                }, {
+                    "id": 3,
+                    "name": "轉帳"
+                }]
+            }
+        }
     }, {
         field: 'tradeDate',
         title: '日期',
-        sortable: true
+        width: 120,
+        sortable: true,
+        editor: {
+            type: 'datebox',
+            options: {
+                currentText: '今天',
+                closeText: '關閉',
+                okText: '確定'
+            }
+        }
     }, {
-        field: 'accountName',
+        field: 'accountId',
         title: '帳戶',
-        sortable: true
+        width: 120,
+        sortable: true,
+        editor: {
+            type: 'combobox',
+            options: {
+                valueField: 'id',
+                textField: 'name',
+                limitToList: true,
+                editable: false,
+                data: [{
+                    "id": 1,
+                    "name": "現金",
+                    "selected": true
+                }, {
+                    "id": 2,
+                    "name": "銀行"
+                }]
+            }
+        }
     }, {
-        field: 'itemName',
+        field: 'itemId',
         title: '項目',
-        sortable: true
+        width: 120,
+        sortable: true,
+        editor: {
+            type: 'combobox',
+            options: {
+                valueField: 'id',
+                textField: 'name',
+                limitToList: true,
+                editable: false,
+                data: [{
+                    "id": 1,
+                    "name": "吃飯",
+                    "selected": true
+                }, {
+                    "id": 2,
+                    "name": "睡覺"
+                }]
+            }
+        }
+    }, {
+        field: 'operate', title: '操作', align: 'center',
+        formatter: function (value, row, index) {
+            if (row.editing) {
+                var saveLink = '<a href="javascript:void(0);" onclick="saveRow(this)">保存</a> ';
+                var cancelLink = '<a href="javascript:void(0);" onclick="cancelRow(this)">取消</a>';
+                return saveLink + ' | ' + cancelLink;
+            } else {
+                var editLink = '<a href="javascript:void(0);" onclick="editRow(this)">修改</a> ';
+                var deleteLink = '<a href="javascript:void(0);" onclick="deleteRow(this)">刪除</a>';
+                return editLink + ' | ' + deleteLink;
+            }
+        }
     }]];
+}
+
+function getRowIndex(target) {
+    var tr = $(target).closest('tr.datagrid-row');
+    return parseInt(tr.attr('datagrid-row-index'));
+}
+
+function saveRow(target) {
+    $('#tradeGrid').datagrid('endEdit', getRowIndex(target));
+}
+
+function cancelRow(target) {
+    $('#tradeGrid').datagrid('cancelEdit', getRowIndex(target));
+}
+
+function editRow(target) {
+    $('#tradeGrid').datagrid('beginEdit', getRowIndex(target));
+}
+
+function deleteRow(target) {
+    // $.messager.confirm('Confirm', 'Are you sure?', function (r) {
+    //     if (r) {
+    $('#tradeGrid').datagrid('deleteRow', getRowIndex(target));
+    //     }
+    // });
 }
