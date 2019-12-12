45cbb7c54f97954b4dc1c7abeb168f9966f5718e
Rex Yu <yu.chenhang@gmail.com>
2019-12-12 13:35:58 +0800

    不繼承 BaseMapper

--- my-dao/src/main/java/com/rex/my/dao/mapper/primary/UserMapper.java
+++ my-dao/src/main/java/com/rex/my/dao/mapper/primary/UserMapper.java
@@ -1,9 +1,20 @@
 package com.rex.my.dao.mapper.primary;
 
-import com.rex.my.dao.mapper.base.BaseMapper;
 import com.rex.my.model.dao.primary.User;
 
-public interface UserMapper extends BaseMapper<User> {
+import java.util.List;
+
+public interface UserMapper {
+
+    int insertSelective(User entity);
+
+    List<User> selectAll();
+
+    User selectByPrimaryKey(String id);
+
+    int update2DeleteByPrimaryKey(String id);
+
+    int updateSelectiveByPrimaryKey(User entity);
 
     User findByEmail(String email);
 
