f421900397e0c8879a5e902db0eef65dc9a68cd7
Rex Yu <yu.chenhang@gmail.com>
2019-12-13 17:19:50 +0800

    取得帳戶含分頁 datagrid 資料 controller 完成

--- my-web/src/main/java/com/rex/my/web/controller/AccountController.java
+++ my-web/src/main/java/com/rex/my/web/controller/AccountController.java
@@ -1,16 +1,39 @@
 package com.rex.my.web.controller;
 
+import com.rex.my.business.service.AccountService;
+import com.rex.my.model.dao.primary.User;
+import com.rex.my.model.easyui.grid.AccountGridVo;
+import com.rex.my.model.easyui.grid.DataGrid;
+import com.rex.my.model.easyui.grid.GridPagination;
+import com.rex.my.web.constant.SessionAttribute;
 import com.rex.my.web.controller.base.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController extends BaseController {
 
+    private AccountService service;
+
+    @Autowired
+    public AccountController(AccountService service) {
+        this.service = service;
+    }
+
     @RequestMapping("/content")
     public String content() {
         return "page/content/account";
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public DataGrid<AccountGridVo> list(GridPagination pagination, HttpSession session) {
+        return new DataGrid<>(service.getAccountsForGrid(pagination, ((User) session.getAttribute(SessionAttribute.USER)).getId()));
     }
 
 }
