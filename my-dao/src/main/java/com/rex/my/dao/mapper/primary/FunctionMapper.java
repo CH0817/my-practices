13b6ecf10f2b1e7116c61d5d63d035a8a6e462a9
Rex Yu <yu.chenhang@gmail.com>
2019-12-16 14:52:14 +0800

    generate function table mapper

---
+++ my-dao/src/main/java/com/rex/my/dao/mapper/primary/FunctionMapper.java
@@ -0,0 +1,7 @@
+package com.rex.my.dao.mapper.primary;
+
+import com.rex.my.dao.mapper.base.BaseMapper;
+import com.rex.my.model.dao.primary.Function;
+
+public interface FunctionMapper extends BaseMapper<Function> {
+}
