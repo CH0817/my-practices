32877e68db779925127e5859d1558f852f23c6ca
Rex Yu <yu.chenhang@gmail.com>
2019-12-18 13:42:15 +0800

    記憶體驗證簡單登入完成

--- my-security/src/main/java/com/rex/my/security/config/WebSecurityConfig.java
+++ my-security/src/main/java/com/rex/my/security/config/WebSecurityConfig.java
@@ -1,37 +1,60 @@
 package com.rex.my.security.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // memory authentication
+        PasswordEncoder encoder = passwordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("test@email.com")
+                .password(encoder.encode("11111111"))
+                .roles("USER", "ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         ignoreH2Console(http);
-        http.authorizeRequests(requests -> requests.antMatchers("/").permitAll());
-        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
-        // http.formLogin(form -> form.loginPage("/").permitAll());
+        // 靜態資源
+        http.authorizeRequests().antMatchers("/js/**", "/easy-ui-1.9.0/**").permitAll();
+        // 不限制請求
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        // role = USER 可使用
+        http.authorizeRequests().antMatchers("/account-book/**", "/account/**", "/combobox/**", "/function/**", "/item/**").hasRole("USER");
+        // 自定義登入
+        http.formLogin().loginPage("/login")// 指定登入頁
+                .usernameParameter("email")// username 與 html email input name 對應
+                .passwordParameter("password")// password 與 html pwd input name 對應
+                .successForwardUrl("/main")// 驗證成功轉跳頁面，不能與 defaultSuccessUrl 、 successHandler 共用
+                .failureForwardUrl("/login-error");// 驗證失敗轉跳頁面
+        // 所有請求均要被驗證，anyRequest()必須在最後否則antMatchers()會出錯
+        http.authorizeRequests().anyRequest().authenticated().and();
     }
 
     private void ignoreH2Console(HttpSecurity http) throws Exception {
         //允許同源使用 iframe，處理 H2 console 的 X-Frame-Options:DENY 問題
-        http.headers().frameOptions().sameOrigin().and()
-                //忽略 H2 console 的 CSRF 驗證
-                .csrf().ignoringAntMatchers("/h2-console/**").and();
+        http.headers().frameOptions().sameOrigin().and();
+        //忽略 H2 console 的 CSRF 驗證
+        http.csrf().ignoringAntMatchers("/h2-console/**").and();
         //H2 console 不須驗證
         http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/easy-ui-1.9.0/**"); // ignore files
-        web.ignoring().antMatchers("/js/**"); // ignore files
     }
 
 }
--- my-web/src/main/java/com/rex/my/web/controller/LoginController.java
+++ my-web/src/main/java/com/rex/my/web/controller/LoginController.java
@@ -1,40 +1,37 @@
 package com.rex.my.web.controller;
 
-import com.rex.my.business.service.LoginService;
-import com.rex.my.business.service.UserService;
-import com.rex.my.model.vo.Login;
-import com.rex.my.web.constant.SessionAttribute;
 import com.rex.my.web.controller.base.BaseController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpSession;
-import java.util.Objects;
 
 @Controller
 public class LoginController extends BaseController {
 
-    private LoginService loginService;
-    private UserService userService;
-
-    @Autowired
-    public LoginController(LoginService loginService, UserService userService) {
-        this.loginService = loginService;
-        this.userService = userService;
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/login";
     }
 
+    /**
+     * 轉向 login.html，同時也是 security 的 loginPage()
+     *
+     * @return login.html
+     */
     @RequestMapping("/login")
-    public String login(HttpSession session, RedirectAttributes redirectAttributes, @Validated Login login) {
-        logger.info("login: {}", login);
-        if (!loginService.login(login)) {
-            redirectAttributes.addFlashAttribute("message", "帳號密碼錯誤");
-            return "redirect:/";
-        }
-        session.setAttribute(SessionAttribute.USER, userService.findByEmail(login.getEmail()));
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/main")
+    public String main() {
         return "page/main";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "帳號或密碼錯誤");
+        return "redirect:/login";
     }
 
     @RequestMapping("/logout")
