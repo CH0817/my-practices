92f84a7ee1e214762ce5169b3cf7b6ff62e61389
Rex Yu <yu.chenhang@gmail.com>
2019-12-19 11:31:15 +0800

    當使用者登入成功後，在進入 "/" 或 "/login" 的時候直接轉到 "/main"

--- my-web/src/main/java/com/rex/my/web/controller/LoginController.java
+++ my-web/src/main/java/com/rex/my/web/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController extends BaseController {
 
@@ -19,7 +21,10 @@ public class LoginController extends BaseController {
      * @return login.html
      */
     @RequestMapping("/login")
-    public String login() {
+    public String login(HttpServletRequest request) {
+        if (request.isUserInRole("USER")) {
+            return "redirect:/main";
+        }
         return "login";
     }
 
