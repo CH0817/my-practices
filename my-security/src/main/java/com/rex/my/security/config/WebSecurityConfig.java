f3c5624554e667ddcaa5a4aaba46ac31339097d5
Rex Yu <yu.chenhang@gmail.com>
2019-12-19 11:15:53 +0800

    code format

--- my-security/src/main/java/com/rex/my/security/config/WebSecurityConfig.java
+++ my-security/src/main/java/com/rex/my/security/config/WebSecurityConfig.java
@@ -32,10 +32,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("email")// username 與 html email input name 對應
                 .passwordParameter("password")// password 與 html pwd input name 對應
                 .defaultSuccessUrl("/main")// 驗證成功轉跳頁面，不能與 successForwardUrl、successHandler 共用
-                .failureForwardUrl("/login-error")
-                .and();// 驗證失敗轉跳頁面
+                .failureForwardUrl("/login-error")// 驗證失敗轉跳頁面
+                .and();
         // 登出
-        http.logout().logoutUrl("/logout").logoutSuccessUrl("/login").and();
+        http.logout().logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .and();
         // 所有請求均要被驗證，anyRequest()必須在最後否則antMatchers()會出錯
         http.authorizeRequests().anyRequest().authenticated().and();
     }
