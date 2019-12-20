4bce9e82337c31d5d60861ff926bed761a36480d
Rex Yu <yu.chenhang@gmail.com>
2019-12-20 17:11:31 +0800

    MyBatis config 重構，減少重複的程式碼

--- my-dao/src/main/java/com/rex/my/dao/config/PrimaryMyBatisConfig.java
+++ my-dao/src/main/java/com/rex/my/dao/config/PrimaryMyBatisConfig.java
@@ -1,31 +1,21 @@
 package com.rex.my.dao.config;
 
-import com.atomikos.icatch.jta.UserTransactionImp;
-import com.atomikos.icatch.jta.UserTransactionManager;
-import org.apache.ibatis.logging.log4j.Log4jImpl;
+import com.rex.my.dao.config.base.BaseMyBatisConfig;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
 import org.springframework.context.annotation.*;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.jta.JtaTransactionManager;
 
 import javax.sql.DataSource;
-import javax.transaction.TransactionManager;
-import javax.transaction.UserTransaction;
-import java.util.Properties;
+import javax.transaction.SystemException;
 
 @Configuration
 @MapperScan(basePackages = {"com.rex.my.dao.mapper.primary"}, sqlSessionTemplateRef = "primarySqlSessionTemplate")
-public class PrimaryMyBatisConfig {
+public class PrimaryMyBatisConfig extends BaseMyBatisConfig {
 
     @Value("${atomikos.datasource.primary.xa-data-source-class-name}")
     private String className;
@@ -36,42 +26,17 @@ public class PrimaryMyBatisConfig {
     @Value("${atomikos.datasource.primary.xa-properties.password}")
     private String password;
 
-    @Bean(name = "primaryAtomikosTransactionManager")
-    @Primary
-    public TransactionManager atomikosTransactionManager() {
-        UserTransactionManager userTransactionManager = new UserTransactionManager();
-        userTransactionManager.setForceShutdown(false);
-        return userTransactionManager;
-    }
-
-    @Bean(name = "primaryUserTransaction")
-    @Primary
-    public UserTransaction userTransaction() throws Throwable {
-        UserTransactionImp userTransactionImp = new UserTransactionImp();
-        userTransactionImp.setTransactionTimeout(10000);
-        return userTransactionImp;
-    }
-
-    @DependsOn({"primaryUserTransaction", "primaryAtomikosTransactionManager"})
     @Bean(name = "primaryTxManager")
     @Primary
-    public PlatformTransactionManager transactionManager(@Qualifier("primaryUserTransaction") UserTransaction userTransaction,
-                                                         @Qualifier("primaryAtomikosTransactionManager") TransactionManager atomikosTransactionManager) {
-        return new JtaTransactionManager(userTransaction, atomikosTransactionManager);
+    public PlatformTransactionManager transactionManager() throws SystemException {
+        return defaultTransactionManager();
     }
 
     @DependsOn({"primaryTxManager"})
     @Bean(name = "primaryDataSource")
     @Primary
     public DataSource dataSource() {
-        AtomikosDataSourceBean result = new AtomikosDataSourceBean();
-        result.setXaDataSourceClassName(className);
-        Properties properties = new Properties();
-        properties.put("URL", url);
-        properties.put("user", username);
-        properties.put("password", password);
-        result.setXaProperties(properties);
-        return result;
+        return createAtomikosDataSource(className, url, username, password);
     }
 
     @DependsOn({"primaryDataSource"})
@@ -79,12 +44,7 @@ public class PrimaryMyBatisConfig {
     @Primary
     public SqlSessionFactory sqlSessionFactory(@Qualifier("primaryDataSource") DataSource primaryDataSource)
             throws Exception {
-        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(primaryDataSource);
-        sessionFactory.setMapperLocations(
-                new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/primary/*.xml"));
-        sessionFactory.setConfiguration(getMyBatisConfiguration());
-        return sessionFactory.getObject();
+        return createSqlSessionFactory(primaryDataSource, "mybatis/mapper/primary/*.xml");
     }
 
     @DependsOn({"primarySqlSessionFactory"})
@@ -95,32 +55,12 @@ public class PrimaryMyBatisConfig {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
-    public org.apache.ibatis.session.Configuration getMyBatisConfiguration() {
-        org.apache.ibatis.session.Configuration result = new org.apache.ibatis.session.Configuration();
-        result.setLogImpl(Log4jImpl.class);
-        return result;
-    }
-
     @Profile({"dev", "test"})
-    @DependsOn({"primaryDataSource", "primaryResourceDatabasePopulator"})
+    @DependsOn("primaryDataSource")
     @Bean(name = "primaryDataSourceInitializer")
     @Primary
-    public DataSourceInitializer dataSourceInitializer(@Qualifier("primaryDataSource") DataSource datasource,
-                                                       @Qualifier("primaryResourceDatabasePopulator") ResourceDatabasePopulator resourceDatabasePopulator) {
-        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(datasource);
-        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-        return dataSourceInitializer;
-    }
-
-    @Profile({"dev", "test"})
-    @Bean(name = "primaryResourceDatabasePopulator")
-    @Primary
-    public ResourceDatabasePopulator resourceDatabasePopulator() {
-        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-        resourceDatabasePopulator.addScript(new ClassPathResource("db/schema-primary-dev.sql"));
-        resourceDatabasePopulator.addScript(new ClassPathResource("db/data/data-primary-dev.sql"));
-        return resourceDatabasePopulator;
+    public DataSourceInitializer dataSourceInitializer(@Qualifier("primaryDataSource") DataSource datasource) {
+        return createDataSourceInitializer(datasource, "db/schema-primary-dev.sql", "db/data/data-primary-dev.sql");
     }
 
 }
--- my-dao/src/main/java/com/rex/my/dao/config/SecondaryMyBatisConfig.java
+++ my-dao/src/main/java/com/rex/my/dao/config/SecondaryMyBatisConfig.java
@@ -1,34 +1,24 @@
 package com.rex.my.dao.config;
 
-import com.atomikos.icatch.jta.UserTransactionImp;
-import com.atomikos.icatch.jta.UserTransactionManager;
-import org.apache.ibatis.logging.log4j.Log4jImpl;
+import com.rex.my.dao.config.base.BaseMyBatisConfig;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.jta.JtaTransactionManager;
 
 import javax.sql.DataSource;
-import javax.transaction.TransactionManager;
-import javax.transaction.UserTransaction;
-import java.util.Properties;
+import javax.transaction.SystemException;
 
 @Configuration
 @MapperScan(basePackages = {"com.rex.my.dao.mapper.secondary"}, sqlSessionFactoryRef = "secondarySqlSessionFactory")
-public class SecondaryMyBatisConfig {
+public class SecondaryMyBatisConfig extends BaseMyBatisConfig {
 
     @Value("${atomikos.datasource.secondary.xa-data-source-class-name}")
     private String className;
@@ -39,50 +29,22 @@ public class SecondaryMyBatisConfig {
     @Value("${atomikos.datasource.secondary.xa-properties.password}")
     private String password;
 
-    @Bean(name = "secondaryAtomikosTransactionManager")
-    public TransactionManager atomikosTransactionManager() {
-        UserTransactionManager userTransactionManager = new UserTransactionManager();
-        userTransactionManager.setForceShutdown(false);
-        return userTransactionManager;
-    }
-
-    @Bean(name = "secondaryUserTransaction")
-    public UserTransaction userTransaction() throws Throwable {
-        UserTransactionImp userTransactionImp = new UserTransactionImp();
-        userTransactionImp.setTransactionTimeout(10000);
-        return userTransactionImp;
-    }
-
-    @DependsOn({"secondaryUserTransaction", "secondaryAtomikosTransactionManager"})
     @Bean(name = "secondaryTxManager")
-    public PlatformTransactionManager transactionManager(@Qualifier("secondaryUserTransaction") UserTransaction userTransaction,
-                                                         @Qualifier("secondaryAtomikosTransactionManager") TransactionManager atomikosTransactionManager) {
-        return new JtaTransactionManager(userTransaction, atomikosTransactionManager);
+    public PlatformTransactionManager transactionManager() throws SystemException {
+        return defaultTransactionManager();
     }
 
     @DependsOn({"secondaryTxManager"})
     @Bean(name = "secondaryDataSource")
     public DataSource dataSource() {
-        AtomikosDataSourceBean result = new AtomikosDataSourceBean();
-        result.setXaDataSourceClassName(className);
-        Properties properties = new Properties();
-        properties.put("URL", url);
-        properties.put("user", username);
-        properties.put("password", password);
-        result.setXaProperties(properties);
-        return result;
+        return createAtomikosDataSource(className, url, username, password);
     }
 
     @DependsOn({"secondaryDataSource"})
     @Bean(name = "secondarySqlSessionFactory")
     public SqlSessionFactory sqlSessionFactory(@Qualifier("secondaryDataSource") DataSource secondaryDataSource)
             throws Exception {
-        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(secondaryDataSource);
-        sessionFactory.setMapperLocations(
-                new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/secondary/*.xml"));
-        sessionFactory.setConfiguration(getMyBatisConfiguration());
-        return sessionFactory.getObject();
+        return createSqlSessionFactory(secondaryDataSource, "mybatis/mapper/secondary/*.xml");
     }
 
     @DependsOn({"secondarySqlSessionFactory"})
@@ -92,30 +54,11 @@ public class SecondaryMyBatisConfig {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
-    public org.apache.ibatis.session.Configuration getMyBatisConfiguration() {
-        org.apache.ibatis.session.Configuration result = new org.apache.ibatis.session.Configuration();
-        result.setLogImpl(Log4jImpl.class);
-        return result;
-    }
-
     @Profile({"dev", "test"})
-    @DependsOn({"secondaryDataSource", "secondaryResourceDatabasePopulator"})
+    @DependsOn("secondaryDataSource")
     @Bean(name = "secondaryDataSourceInitializer")
-    public DataSourceInitializer dataSourceInitializer(@Qualifier("secondaryDataSource") DataSource datasource,
-                                                       @Qualifier("secondaryResourceDatabasePopulator") ResourceDatabasePopulator resourceDatabasePopulator) {
-        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(datasource);
-        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-        return dataSourceInitializer;
-    }
-
-    @Profile({"dev", "test"})
-    @Bean(name = "secondaryResourceDatabasePopulator")
-    public ResourceDatabasePopulator resourceDatabasePopulator() {
-        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-        resourceDatabasePopulator.addScript(new ClassPathResource("db/schema-secondary-dev.sql"));
-        resourceDatabasePopulator.addScript(new ClassPathResource("db/data/data-secondary-dev.sql"));
-        return resourceDatabasePopulator;
+    public DataSourceInitializer dataSourceInitializer(@Qualifier("secondaryDataSource") DataSource datasource) {
+        return createDataSourceInitializer(datasource, "db/schema-secondary-dev.sql", "db/data/data-secondary-dev.sql");
     }
 
 }
