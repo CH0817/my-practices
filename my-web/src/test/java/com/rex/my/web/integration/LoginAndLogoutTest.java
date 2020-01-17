f551d4e9cb415a5394a2469c28c556ad39b79133
Rex Yu <yu.chenhang@gmail.com>
2020-01-17 11:21:04 +0800

    移除 Thread.sleep() 改用 WebDriverWait

--- my-web/src/test/java/com/rex/my/web/integration/LoginAndLogoutTest.java
+++ my-web/src/test/java/com/rex/my/web/integration/LoginAndLogoutTest.java
@@ -6,18 +6,26 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
 public class LoginAndLogoutTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void setDriver() {
         // 設置 chrome driver .exe 所在的環境變數
         System.setProperty("webdriver.chrome.driver", "D:/IDE/Selenium/Browers/Chrome/version 79/chromedriver.exe");
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 3L);
+        // 隱式 wait
+        driver.manage().timeouts().implicitlyWait(3L, TimeUnit.SECONDS);
         // 開啟網頁
         driver.get("http://localhost:8080/my-web/login");
     }
@@ -29,22 +37,23 @@ public class LoginAndLogoutTest {
     }
 
     @Test
-    public void loginAndLogout() throws Exception {
+    public void loginAndLogout() {
         login();
         logout();
     }
 
-    private void login() throws Exception {
+    private void login() {
         // 輸入 email
         driver.findElement(By.xpath("//*[@id='email']/following-sibling::span/input[1]")).sendKeys("test@email.com");
         // 輸入 password
         driver.findElement(By.xpath("//*[@id='password']/following-sibling::span/input[1]")).sendKeys("11111111");
-        // 停頓 0.5 秒，等待 EasyUI 反應
-        Thread.sleep(500);
-        // 找到 #loginBtn button 並執行 click event
-        driver.findElement(By.id("loginBtn")).click();
-        // 驗證
-        assertEquals("/main", driver.getCurrentUrl().substring(driver.getCurrentUrl().lastIndexOf("/")));
+        // 等待 easyui box 的錯誤消失
+        if (wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.className("tooltip-content"))))) {
+            // 找到 #loginBtn button 並執行 click event
+            driver.findElement(By.id("loginBtn")).click();
+            // 驗證
+            assertEquals("/main", driver.getCurrentUrl().substring(driver.getCurrentUrl().lastIndexOf("/")));
+        }
     }
 
     public void logout() {
--- my-web/src/test/java/com/rex/my/web/integration/LoginFormVerifyTest.java
+++ my-web/src/test/java/com/rex/my/web/integration/LoginFormVerifyTest.java
@@ -7,21 +7,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LoginFormVerifyTest {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void setDriver() {
         // 設置 chrome driver .exe 所在的環境變數
         System.setProperty("webdriver.chrome.driver", "D:/IDE/Selenium/Browers/Chrome/version 79/chromedriver.exe");
         driver = new ChromeDriver();
-        // driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 3L);
+        // 隱式 wait
+        driver.manage().timeouts().implicitlyWait(3L, TimeUnit.SECONDS);
         // 開啟網頁
         driver.get("http://localhost:8080/my-web/login");
     }
@@ -33,57 +39,51 @@ public class LoginFormVerifyTest {
     }
 
     @Test
-    public void loginFailure() throws InterruptedException {
+    public void loginFailure() {
         // 輸入 email
         typeEmail("test11111@email.com");
         // 輸入 password
         typePassword("11111111");
-        // 停頓 0.5 秒，等待 EasyUI 反應
-        Thread.sleep(500);
-        // 找到 #loginBtn button 並執行 click event
-        driver.findElement(By.id("loginBtn")).click();
-        // 驗證 server 回傳的錯誤訊息
-        assertEquals("帳號或密碼錯誤", driver.findElement(By.xpath("//*[@id='loginWindow']/div[1]")).getText());
+        // 等待 easyui box 的錯誤消失
+        if (wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.className("tooltip-content"))))) {
+            // 找到 #loginBtn button 並執行 click event
+            driver.findElement(By.id("loginBtn")).click();
+            // 驗證 server 回傳的錯誤訊息
+            WebElement errorMessageDiv = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id='loginWindow']/div[1]"))));
+            assertEquals("帳號或密碼錯誤", errorMessageDiv.getText());
+        }
     }
 
     @Test
-    public void errorEmailFormat() throws InterruptedException {
+    public void errorEmailFormat() {
         // 輸入 email
         typeEmail("111@11");
-        // 停頓 0.5 秒，等待 EasyUI 反應
-        Thread.sleep(500);
         // 驗證 EasyUI 錯誤訊息
-        assertEquals("請輸入有效的電子郵件地址", driver.findElement(By.className("tooltip-content")).getText());
+        assertTrue(verifyInputErrorMessage("請輸入有效的電子郵件地址"));
     }
 
     @Test
-    public void errorPasswordFormat() throws InterruptedException {
+    public void errorPasswordFormat() {
         // 輸入 password
         typePassword("1111");
-        // 停頓 0.5 秒，等待 EasyUI 反應
-        Thread.sleep(500);
         // 驗證 EasyUI 錯誤訊息
-        assertEquals("密碼必須是8~12碼", driver.findElement(By.className("tooltip-content")).getText());
+        assertTrue(verifyInputErrorMessage("密碼必須是8~12碼"));
     }
 
     @Test
-    public void emptyEmail() throws InterruptedException {
+    public void emptyEmail() {
         // 輸入 email
         typeEmail("");
-        // 停頓 0.5 秒，等待 EasyUI 反應
-        Thread.sleep(500);
         // 驗證 EasyUI 錯誤訊息
-        assertEquals("請輸入Email", driver.findElement(By.className("tooltip-content")).getText());
+        assertTrue(verifyInputErrorMessage("請輸入Email"));
     }
 
     @Test
-    public void emptyPassword() throws InterruptedException {
+    public void emptyPassword() {
         // 輸入 password
         typePassword("");
-        // 停頓 0.5 秒，等待 EasyUI 反應
-        Thread.sleep(500);
         // 驗證 EasyUI 錯誤訊息
-        assertEquals("請輸入密碼", driver.findElement(By.className("tooltip-content")).getText());
+        assertTrue(verifyInputErrorMessage("請輸入密碼"));
     }
 
     private void typeEmail(String email) {
@@ -99,6 +99,10 @@ public class LoginFormVerifyTest {
         // 先清空再重新輸入
         input.sendKeys("");
         input.sendKeys(value);
+    }
+
+    private boolean verifyInputErrorMessage(String message) {
+        return wait.until(ExpectedConditions.textToBe(By.className("tooltip-content"), message));
     }
 
 }
