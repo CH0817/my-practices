6f8a0ff48f660e3538c360dffdfb999c6fa25519
Rex Yu <yu.chenhang@gmail.com>
2020-02-06 13:53:05 +0800

    刪除 service layer 的 method validation

--- src/main/java/com/rex/practice/service/ItemService.java
+++ src/main/java/com/rex/practice/service/ItemService.java
@@ -4,20 +4,14 @@ import com.github.pagehelper.PageInfo;
 import com.rex.practice.dao.model.primary.Item;
 import com.rex.practice.model.easyui.grid.GridPagination;
 import com.rex.practice.model.easyui.grid.ItemGridVo;
-import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
-@Validated
 public interface ItemService {
 
     PageInfo<ItemGridVo> getItemsForGrid(GridPagination pagination, String userId);
 
-    <E extends Item> String save(@NotNull E entity);
+    <E extends Item> String save(E entity);
 
-    boolean updateToDeleteByIds(@NotEmpty String[] ids, @NotBlank String userId);
+    boolean updateToDeleteByIds(String[] ids, String userId);
 
     <E extends Item> Boolean updateById(E entity);
 
