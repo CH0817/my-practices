51427ac8603761c37da64bd84edf2e8a8bdaae82
Rex Yu <yu.chenhang@gmail.com>
2020-02-12 11:28:57 +0800

    處理打包成 war 無法執行的問題

--- src/main/java/com/rex/practice/PracticeApplication.java
+++ src/main/java/com/rex/practice/PracticeApplication.java
@@ -2,12 +2,19 @@ package com.rex.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class PracticeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PracticeApplication.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+        return springApplicationBuilder.sources(PracticeApplication.class);
     }
 
 }
